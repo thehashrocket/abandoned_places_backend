2c975c828d868e826389af9f30a0395ed8eba7f8
Jason Shultz <jasoneshultz@icloud.com>
2019-06-16 20:56:35 -0600

    backend work

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,123 @@
-const mutations = {};
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { randomBytes } = require('crypto');
+const { promisify } = require('util');
+const { transport, makeANiceEmail } = require('../mail');
+
+const mutations = {
+  async signup(parent, args, ctx, info) {
+    // lowercase their email
+    args.email = args.email.toLowerCase();
+    // hash their password
+    const password = await bcrypt.hash(args.password, 10);
+    // create the user in the database
+    const user = await ctx.db.mutation.createUser({
+      data: {
+        ...args,
+        password,
+        permissions: { set: ['USER'] }
+      }
+    }, info);
+    // create the JWT token
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    // we set the jwt as a cookie on the response
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
+    });
+    // Return user to the browser
+    return user;
+  },
+  async signin(parent, { email, password }, ctx, info) {
+    // check if the there is a user with that email.
+    email = email.toLowerCase();
+    const user = await ctx.db.query.user({ where: { email: email } });
+    if (!user) {
+      throw new Error(`No such user for email ${email}`);
+    }
+    // check if there password is correct
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid) {
+      throw new Error('Invalid password!');
+    }
+    // generate the JWT Token
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    // Set the cookie
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
+    });
+    // Return user to the browser
+    return user;
+  },
+  async signout(parent, args, ctx, info) {
+    ctx.response.clearCookie('token');
+    return { message: 'Goodbye!' };
+  },
+  async requestReset(parent, args, ctx, info) {
+    // check if this is a real user
+    const user = await ctx.db.query.user({ where: { email: args.email } })
+    if (!user) {
+      throw new Error(`No such user for email ${args.email}`);
+    }
+    // set a reset token and expiry on that user
+    const randomBytesPromisified = promisify(randomBytes);
+    const resetToken = (await randomBytesPromisified(20)).toString('hex');
+    const resetTokenExpiry = Date.now() + 3600000; // 1 hour from now.
+    const res = await ctx.db.mutation.updateUser({
+      where: { email: args.email },
+      data: { resetToken, resetTokenExpiry },
+    });
+
+    // email user a reset token
+    const mailRes = await transport.sendMail({
+      from: 'some@guy.com',
+      to: user.email,
+      subject: 'Your password reset token',
+      html: makeANiceEmail(`Your password reset Token is here:
+      \n\n
+      <a href = "${process.env.FRONTEND_URL}/reset?resetToken=${resetToken}">Click Here to Reset</a>`)
+    })
+    // Return the message
+    return { message: 'Thanks!' }
+  },
+  async resetPassword(parent, args, ctx, info) {
+    // check if the password match
+    if (args.password !== args.confirmPassword) {
+      throw new Error('Your password do not match!');
+    }
+    // check if its a legit reset token
+    // check if it's expired
+    const [user] = await ctx.db.query.users({
+      where: {
+        resetToken: args.resetToken,
+        resetTokenExpiry_gte: Date.now() - 3600000
+      }
+    })
+    if (!user) {
+      throw new Error('This token is either invalid or expired');
+    }
+    // hash their new password
+    const password = await bcrypt.hash(args.password, 10);
+    // save the new password to the user and remove old resetToken fields.
+    const updatedUser = await ctx.db.mutation.updateUser({
+      where: { email: user.email },
+      data: {
+        password,
+        resetToken: null,
+        resetTokenExpiry: null,
+      }
+    })
+    // Generate JWT
+    const token = jwt.sign({ userId: updatedUser.id }, process.env.APP_SECRET);
+    // Set the JWT cookie
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
+    });
+    // return the new user
+    return updatedUser;
+  }
+};
 
 module.exports = mutations;
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,3 +1,32 @@
-const Query = {};
+const { forwardTo } = require('prisma-binding');
+const { hasPermission } = require('../utils');
+const Query = {
+  // async items(parent, args, ctx, info) {
+  //   const items = await ctx.db.query.items();
+  //   return items;
+  // }
+  me(parent, args, ctx, info) {
+    // check if there is a current user ID
+    if (!ctx.request.userId) {
+      return null;
+    }
+    return ctx.db.query.user({
+      where: { id: ctx.request.userId },
+    }, info);
+  },
+
+  async users(parent, args, ctx, info) {
+    // check if they are logged in
+    if (!ctx.request.userId) {
+      throw new Error('You must be logged in!');
+    }
+    // 1. Check if the user has the permissions to query all the users.
+    hasPermission(ctx.request.user, ['ADMIN', 'PERMISSIONUPDATE']);
+    // 2. If they do, query all the users!
+    return ctx.db.query.users({}, info);
+  },
+
+
+};
 
 module.exports = Query;
