0b05b620994f919655f39e9fcdb491015b89d4c6
Jason Shultz <jasoneshultz@icloud.com>
2019-06-26 17:40:22 -0600

    implemented permission filtering

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -25,11 +25,32 @@ const Query = {
       throw new Error('You must be logged in!');
     }
     // 1. Check if the user has the permissions to query all the users.
-    // hasPermission(ctx.request.user, ['ADMIN', 'PERMISSIONUPDATE']);
+    hasPermission(ctx.request.user, ['ADMIN', 'PERMISSIONUPDATE']);
     // 2. If they do, query all the users!
     return ctx.db.query.users({}, info);
   },
 
+  async hasPermissions(parent, args, ctx, info) {
+    // check if they are logged in
+    if (!ctx.request.userId) {
+      throw new Error('You must be logged in!');
+    }
+
+    if (!args['permissions']) {
+      throw new Error('No Permissions provided.');
+    }
+    perms = args['permissions'].split(',');
+
+    const matchedPermissions = ctx.request.user.permissions.filter(permissionTheyHave =>
+      perms.includes(permissionTheyHave)
+    );
+
+    if (!matchedPermissions.length) {
+      return false;
+    } else {
+      return ctx.request.user
+    }
+  }
 
 };
 
