ecc14764cf6e2cc336d890b5116ba591d8fd4a34
Jason Shultz <jasoneshultz@icloud.com>
2019-06-27 05:50:47 -0600

    still..

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -21,9 +21,9 @@ const Query = {
 
   async users(parent, args, ctx, info) {
     // check if they are logged in
-    if (!ctx.request.userId) {
-      throw new Error('You must be logged in!');
-    }
+    // if (!ctx.request.userId) {
+    //   throw new Error('You must be logged in!');
+    // }
     // 1. Check if the user has the permissions to query all the users.
     // hasPermission(ctx.request.user, ['ADMIN', 'PERMISSIONUPDATE']);
     // 2. If they do, query all the users!
